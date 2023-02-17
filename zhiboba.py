046abb85c590ae80dec8064df9510d7510c4f5c0
chenpeng880126 <white.chen@qq.com>
2023-02-17 15:39:26 +0800

    test

--- zhiboba.py
+++ zhiboba.py
@@ -64,6 +64,7 @@ if __name__ == '__main__':
     r = requests.get(url, headers=headers)
     analysis_zhiboba(r.json(), t1_datetime)
 
+    #test
     '''
     with open('data/zhiboba1', "w+", encoding="utf-8") as f:
         f.write(str(r.json()))
